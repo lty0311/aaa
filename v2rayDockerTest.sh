2b45f2daa66f5f816bfb591b8556920e410610a5
houdawei <houdawei@haohantech.net>
2022-12-11 16:09:25 +0800

    提交测试版

--- v2rayDockerTest.sh
+++ v2rayDockerTest.sh
@@ -7,9 +7,8 @@ BEGIN_PATH=$(pwd)
 
 #定义操作变量, 0为否, 1为是
 HELP=0
-
 CHINESE=0
-
+ISHAVEDOCKER=0
 v2ray_domain=""
 
 
@@ -39,26 +38,38 @@ installFinish() {
     
     #回到原点
     cd ${BEGIN_PATH}
-    echo -e "\n请在安全组放开端口 12345\n"
+    echo -e "\n请在安全组放开端口 6810\n"
 }
 
+function docker_is_install()
+{
+    colorEcho "正在检查Docker是否安装.....\n"
+    docker -v
+    if [ $? -eq  0 ]; then
+        ISHAVEDOCKER=1
+    fi
+}
 
 main() {
     
     colorEcho ${BLUE} "开始下载Docker..\n"
     
-    yum install -y amazon-linux-extras yum-utils device-mapper-persistent-data lvm2
-    yum-config-manager --enable extras
-    amazon-linux-extras install docker
-    usermod -a -G docker ec2-user
-    systemctl enable docker.service
-    systemctl start docker.service
+    docker_is_install
+    
+    if [[ $ISHAVEDOCKERn == 0 ]]; then
+        yum install -y amazon-linux-extras yum-utils device-mapper-persistent-data lvm2
+        yum-config-manager --enable extras
+        amazon-linux-extras install docker
+        usermod -a -G docker ec2-user
+        systemctl enable docker.service
+        systemctl start docker.service
+    fi
     
     
     colorEcho ${BLUE} "开始下载Docker镜像..\n"
     
     docker pull feihuwangluo/v2ray_hdw:v2
-    docker run -d --name v2ray -p 12345:12345 --privileged --restart always feihuwangluo/v2ray_hdw
+    docker run -d --name v2ray -p 6810:12345 --privileged --restart always feihuwangluo/v2ray_hdw
     
     colorEcho ${RED} "安装完成！\n"
     
